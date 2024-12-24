567adc90a85a9969cd45dec568419884e049d73a
Kanhagarg7 <162786866+Kanhagarg7@users.noreply.github.com>
2024-12-25 01:36:12 +0530

    Update config.py

--- Pyrostring/config.py
+++ Pyrostring/config.py
@@ -2,6 +2,6 @@
 CONFIG GENERATOR
 """
 
-API_ID   : int = 101010
-API_HASH : str = 'abcdefghijklmnopqrstuv'
+API_ID   : int = 6
+API_HASH : str = 'eb06d4abfb49dc3eeb1aeb98ae0f581e'
 #you can get this on my.telegram.org
